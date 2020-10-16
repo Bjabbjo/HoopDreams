fca24600407c9c6aff5039f5a91329aca7d3fbc2
Bjabbjo <42970264+Bjabbjo@users.noreply.github.com>
2020-10-16 17:47:51 +0000

    Update index.js

--- template/resolvers/index.js
+++ template/resolvers/index.js
@@ -2,6 +2,8 @@ const basketballFieldResolver = require("./basketballFieldResolver");
 const pickupGamesResolver = require("./pickupGamesResolver");
 const playerResolver = require("./playerResolver");
 const moment = require("./moment");
+const playerMutations = require("../schema/mutations/playerMutations");
+const pickupGameMutations = require("../schema/mutations/pickupGameMutations");
 
 module.exports = {
     Query: {
@@ -10,7 +12,8 @@ module.exports = {
         ...playerResolver.queries
     },
     Mutation: {
-
+        ...playerMutations,
+        ...pickupGameMutations
     },
     Moment: {
         ...moment
